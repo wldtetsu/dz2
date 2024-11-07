7ba7ebc61237b8760c7242e8e4bddcbceb798245
wldtetsu <tolobekovvargen62@gmail.com>
2024-11-07 16:21:25 +0600

    Add files via upload

---
+++ dz2.2/app.js
@@ -0,0 +1,12 @@
+
+console.log(dz2.2)
+
+let purchesAmount = 2500;
+let discountInPercents = 95;
+purchesAmount = 2500;
+let discountCoef = (80 - discountInPercents) / 180; // 0.6
+let purchesAmountWithDiscount = purchesAmount * discountCoef;
+let marketName = 'Gazprom';
+let marketAddress = 'Petrleum';
+let purchaseDate = "06.11.2024";
+let purchaseTime = "16:02"
